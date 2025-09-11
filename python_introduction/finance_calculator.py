395ebab14054e53374f8b7dcb50812f15564f37b
AnastaseNtez <anastasentezi@gmail.com>
2025-09-11 19:49:45 +0200

    fixing a bug in a formula

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 #This script will calculate the user’s monthly savings based on inputted monthly income and expenses
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
-monthly_savings = monthly_income - total_monthly_expenses
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_savings = float(monthly_income - monthly_expenses)
 projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
-print("your monthly savings are $"+str(monthly_savings)+".")
+print("your monthly savings are $"+str(int(monthly_savings))+".")
 print("Projected savings after one year, with interest, is: $"+str(int(projected_savings))+".")
