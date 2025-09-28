c716f57dde60b0a0ec97001f6bb159c42fefdcf5
AnastaseNtez <anastasentezi@gmail.com>
2025-09-28 15:37:38 +0200

    fixing small bug with input message.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,12 +13,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter an item to add to the shopping cart:")
+            item = input("Enter the item to add:")
             shopping_list.append(item)
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter an item to remove from the shopping cart:")
+            item = input("Enter the item to remove:")
             if item in shopping_list:
                 shopping_list.remove(item)
             else:
