7fe2c0b8239be231ade824743112048df6546c0b
AnastaseNtez <anastasentezi@gmail.com>
2025-10-04 15:29:23 +0200

    Ammending the display balance function

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,5 +17,6 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        """Display the current balance in a user-friendly format."""
-        print(f"Current Balance: ${self.account_balance}")
+        #Display the current balance in a user-friendly format with 2 decimals."""
+        print(f"Current Balance: ${self.account_balance:.2f}")
+
